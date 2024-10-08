36265a8430115b5984399864be2c8122fe8c9e70
TUSHAR SINGH <125133044+codegurupoocoo@users.noreply.github.com>
2024-10-08 19:48:55 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+const data=[
+    
+    {name:'tushar', email: 'tushar@gmail.com'},
+    {name:'anil', email: 'anil@gmail.com'},
+    {name:'ji', email: 'ji@gmail.com' , contact:99999999999},
+
+
+
+]
+module.exports=data;
